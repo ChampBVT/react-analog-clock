581001fc86369b6389e3f5c00b4e44b5464f6ac9
champbvt <borvorntat.nir@gmail.com>
2021-04-08 14:21:29 +0700

    init & finish

--- src/App.js
+++ src/App.js
@@ -1,13 +1,41 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState, useEffect} from 'react'
 
 function App() {
+  const [date, setDate] = useState('')
+
+  const createDate = (string) => {
+    return new Date(string)
+  }
+  useEffect(()=>{
+    setInterval(()=>{
+      setDate(new Date())
+    }, 1000)
+  },[]);
+
+  const computeDegreeSec = (date) => {
+    return createDate(date).getSeconds()*6
+  }
+  const computeDegreeMin = (date) => {
+    return createDate(date).getMinutes()*6
+  }
+  const computeDegreeHour = (date) => {
+    return createDate(date).getHours()*15
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <div className="clock">
+          <div className="dot" />
+          <div className="line long" style={{transform: `rotate(${computeDegreeSec(date)}deg)`}} />
+          <div className="line mid" style={{transform: `rotate(${computeDegreeMin(date)}deg)`}} />
+          <div className="line short" style={{transform: `rotate(${computeDegreeHour(date)}deg)`}} />
+        </div>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          React APP Analog Clock with CSS & JS
         </p>
         <a
           className="App-link"
